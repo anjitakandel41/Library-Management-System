89f3dc197b9d1ebd17b75fa846bea4498064ecf7
Anjita <156284237+anjitakandel41@users.noreply.github.com>
2026-08-20 07:13:33 +0545

    Delete Desktop directory

--- Desktop/scam project/chatbot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import chatbot_home
-
-app_name = 'chatbot'
-
-urlpatterns = [
-    path('', chatbot_home, name='home'),
-]
--- Desktop/scam project/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from create-django-project-structure-create-apps!")
-
-
-if __name__ == "__main__":
-    main()
